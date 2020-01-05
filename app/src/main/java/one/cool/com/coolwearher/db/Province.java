5e071b0b938833194717420f59ffbdedddac9629
tom_pro <goodmoring@maomao.com>
2020-01-05 13:19:00 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/one/cool/com/coolwearher/db/Province.java
@@ -0,0 +1,33 @@
+package one.cool.com.coolwearher.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName; //记录省的名字
+    private int provinceCode;//记录省的代号
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId() {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
